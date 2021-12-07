ac223f5e07a5829d9fa3229b0c6c2467ae3bb14f
Mamadou mbaye wade <mamadou.mb.wade@gmail.com>
2021-12-07 00:14:38 +0000

    second commit

---
+++ src/data.js
@@ -0,0 +1,118 @@
+import slide1 from "../src/img/slider/1.png";
+import slide2 from "../src/img/slider/2.png";
+import slide3 from "../src/img/slider/3.png";
+
+import cat1 from "../src/img/category/1.jpeg";
+import cat2 from "../src/img/category/2.jpeg";
+import cat3 from "../src/img/category/3.jpeg";
+
+import prod1 from "../src/img/products/1.png";
+import prod2 from "../src/img/products/2.png";
+import prod3 from "../src/img/products/3.png";
+import prod4 from "../src/img/products/4.png";
+import prod5 from "../src/img/products/5.png";
+import prod6 from "../src/img/products/6.png";
+import prod7 from "../src/img/products/7.png";
+import prod8 from "../src/img/products/8.jpg";
+import prod9 from "../src/img/products/9.jpg";
+import prod10 from "../src/img/products/10.jpeg";
+import prod11 from "../src/img/products/11.jpg";
+import prod12 from "../src/img/products/12.jpeg";
+import prod13 from "../src/img/products/13.jpg";
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: slide1,
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "b2d7f2ff",
+  },
+  {
+    id: 2,
+    img: slide2,
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: slide3,
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: cat1,
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: cat2,
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: cat3,
+    title: "LIGHT JACKETS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: prod1,
+  },
+  {
+    id: 2,
+    img: prod2,
+  },
+  {
+    id: 3,
+    img: prod3,
+  },
+  {
+    id: 4,
+    img: prod4,
+  },
+  {
+    id: 5,
+    img: prod5,
+  },
+  {
+    id: 6,
+    img: prod6,
+  },
+  {
+    id: 7,
+    img: prod7,
+  },
+  {
+    id: 8,
+    img: prod8,
+  },
+  {
+    id: 9,
+    img: prod9,
+  },
+  {
+    id: 10,
+    img: prod10,
+  },
+  {
+    id: 11,
+    img: prod11,
+  },
+  {
+    id: 12,
+    img: prod12,
+  },
+  {
+    id: 13,
+    img: prod13,
+  },
+];
