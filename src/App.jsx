049c618c77fa8b83bb16e76b6c94d8f098672ba8
Mamadou mbaye wade <mamadou.mb.wade@gmail.com>
2021-12-07 14:28:27 +0000

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Cart />
     </div>
   );
 }
