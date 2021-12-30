443d8957f1c59a07b86f8b77b210a82750ca2b05
Mamadou mbaye wade <mamadou.mb.wade@gmail.com>
2021-12-30 21:39:57 +0000

    last commit to client

--- src/requestMethodes.js
+++ src/requestMethodes.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 const Base_URL = "http://localhost:5000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYjE0ODEwZTEyYmYzZWQyNmNiMmI0YSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MDgxODIyMSwiZXhwIjoxNjQxMDc3NDIxfQ._GgppjXKkuSySzdd-KtX1fq_J65Fwn-3JqYJ89p1icw";
-
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+  .currentUser.accessToken;
 export const publicRequest = axios.create({
   baseURL: Base_URL,
 });
